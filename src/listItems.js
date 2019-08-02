58706f407830612aa388cd31eb0c85fc68f98ce3
iosi <ivan.osipov@futurice.com>
2019-08-02 12:52:58 +0300

    refactor how we get stories

--- src/listItems.js
+++ src/listItems.js
@@ -1,27 +1,31 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 export const ListItems = () => {
   const [stories, setStories] = useState([]);
 
-  // Fetch ids and then take first 20 ids and fetch  news stories
   const fetchStories = () => {
-      let newsStoriesArray = [];
-      fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
-          .then(res => res.json())
-          .then(newsIds => newsIds.slice(0, 19))
-          .then(newsIdsArray =>
-              Promise.all(newsIdsArray.map(newsId => fetch(`https://hacker-news.firebaseio.com/v0/item/${newsId}.json `)))
-                  .then(responses => responses.forEach(res => {
-                          res.json()
-                              .then(news => newsStoriesArray.push(news))
-                              .then(newsItems => setStories(newsStoriesArray))
-                      })
-                  ));
+      const topStoriesUrl ='https://hacker-news.firebaseio.com/v0/topstories.json';
+      const storiesBaseUrl = 'https://hacker-news.firebaseio.com/v0/item/';
+
+      // get top stories ids and take first 20 items
+      // then fetch news with those items
+      axios.get(topStoriesUrl)
+          .then(response => response.data.slice(0,19).map(id => {
+              const url = `${storiesBaseUrl}${id}.json`;
+              return axios.get(url);
+          }))
+          .then(promises => axios.all(promises))
+          .then(listOfStories => setStories(listOfStories));
   };
-   // Getting ids when component loads
+
+  // Run fetch stories when component loads
   useEffect(() => {
       fetchStories();
   }, []);
 
-  return <div className="ui link list">fw</div>;
+  return (
+      <h2>Top stories</h2>
+
+  );
 };
