ac3f879cd88c346e1d0de43ee3d03ddab9ecd332
iosi <ivan.osipov@futurice.com>
2019-08-02 11:23:08 +0300

    getting 20 news

--- src/listItems.js
+++ src/listItems.js
@@ -1,28 +1,27 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 export const ListItems = () => {
   const [stories, setStories] = useState([]);
 
-  // Fetch ids and then take first 20 ids and fetch stories
-  const fetchStories = async () => {
-    axios
-      .get("https://hacker-news.firebaseio.com/v0/beststories.json")
-      .then(response => {
-        response.data.slice(0, 19).map(id => {
-          axios
-            .get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-            .then(response => {
-              setStories(response.data);
-            });
-        });
-      });
+  // Fetch ids and then take first 20 ids and fetch  news stories
+  const fetchStories = () => {
+      let newsStoriesArray = [];
+      fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
+          .then(res => res.json())
+          .then(newsIds => newsIds.slice(0, 19))
+          .then(newsIdsArray =>
+              Promise.all(newsIdsArray.map(newsId => fetch(`https://hacker-news.firebaseio.com/v0/item/${newsId}.json `)))
+                  .then(responses => responses.forEach(res => {
+                          res.json()
+                              .then(news => newsStoriesArray.push(news))
+                              .then(newsItems => setStories(newsStoriesArray))
+                      })
+                  ));
   };
-
-  // Getting ids when component loads
+   // Getting ids when component loads
   useEffect(() => {
-    fetchStories();
+      fetchStories();
   }, []);
 
-  return <div className="ui link list">Some</div>;
+  return <div className="ui link list">fw</div>;
 };
